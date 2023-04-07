4ef452c213012f85ab1f6243af89c32c46422a13
ion-dci <ion.lazarev@digitalcareerinstitute.org>
2023-04-07 10:43:09 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>React-couter</h1>
         <Counter start={23} />
       </header>
     </div>
